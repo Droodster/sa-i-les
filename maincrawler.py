ccc85e81573d98ee011cf12f7d923994ca245804
Droodster <42186735+Droodster@users.noreply.github.com>
2023-02-02 02:30:37 +0100

    6

--- maincrawler.py
+++ maincrawler.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import backupai
 from checkcountry import get_language
 
 def maincrawler(url):
@@ -15,6 +16,15 @@ def maincrawler(url):
     # extract the relevant information from the website
     about_us = soup.find("a", string=f"About Us ({lang})")
     contact_us = soup.find("a", string=f"Contact Us ({lang})")
+
+    # if the contact us link was not found, try to get it from the sitemap file
+    if not contact_us:
+        sitemap_file = 'sitemap.xml'
+        _, contact_us_url = backupai.extract_info_from_sitemap(sitemap_file)
+        if contact_us_url:
+            response = requests.get(contact_us_url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            contact_us = soup.find("a", string=f"Contact Us ({lang})")
 
     # return the results
     return (about_us, contact_us)
